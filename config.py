7e5ac65a5bccc03bf6f6985f9ef9df31e081be38
mpamiesi <113771379+mpamiesi@users.noreply.github.com>
2023-03-25 17:49:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 import os
 
 # Configuración del bot de Telegram
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_TOKEN = '6119983782:AAGwSeqDU2E7UfyhIz6t2wh81AGopsMjZx8'
+TELEGRAM_CHAT_ID = 'https://t.me/SignalBot100'
 
 # Configuración de Trading View
-TV_INDICATOR = 'mi-indicador'
+TV_INDICATOR = 'Ampel.inv'
 TV_ASSETS = ['BTC/USD', 'ETH/USD', 'EUR/USD', 'Gold']
+
+# Configuración de los mensajes de alerta
+ALERT_MESSAGE_FORMAT = """
+{pair}
+Precio: {price}
+Tipo de posición: {position_type}
+Temporalidad: {timeframe}
+"""
